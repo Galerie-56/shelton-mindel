aa5e08f67bcad68418adfad04eca1960af6f9597
Alexandra Spalato <alexaspalato@gmail.com>
2024-05-09 17:27:09 +0200

    generate types

--- app/types/component-types-sb.d.ts
+++ app/types/component-types-sb.d.ts
@@ -1,20 +1,35 @@
 import {StoryblokStory} from 'storyblok-generate-ts'
 
-export interface AllPostsStoryblok {
-  headline?: string;
-  intro?: string;
-  grid?: boolean;
+export interface ArchiectEyesStoryblok {
+  items?: ArchitectEyeStoryblok[];
   _uid: string;
-  component: "all-posts";
+  component: "archiect-eyes";
   [k: string]: any;
 }
 
-export interface AllProjectsStoryblok {
-  projects?: ProjectStoryblok[];
-  _uid: string;
-  component: "all-projects";
-  [k: string]: any;
-}
+export type MultilinkStoryblok =
+  | {
+      id?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "story";
+      target?: "_self" | "_blank";
+      [k: string]: any;
+    }
+  | {
+      url?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "asset" | "url";
+      target?: "_self" | "_blank";
+      [k: string]: any;
+    }
+  | {
+      email?: string;
+      linktype?: "email";
+      target?: "_self" | "_blank";
+      [k: string]: any;
+    };
 
 export interface AssetStoryblok {
   _uid?: string;
@@ -34,38 +49,34 @@ export interface AssetStoryblok {
   [k: string]: any;
 }
 
-export interface AuthorStoryblok {
-  avatar?: AssetStoryblok;
-  bio?: string;
-  seo_plugin?: {
-    _uid?: string;
-    title?: string;
-    plugin?: string;
-    og_image?: string;
-    og_title?: string;
-    description?: string;
-    twitter_image?: string;
-    twitter_title?: string;
-    og_description?: string;
-    twitter_description?: string;
-    [k: string]: any;
-  };
-  twitter?: string;
-  seo?: SeoStoryblok[];
-  grid?: boolean;
+export interface ArchitectEyeStoryblok {
+  title?: string;
+  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  image?: AssetStoryblok;
+  description?: string;
   _uid: string;
-  component: "author";
+  component: "architect-eye";
   [k: string]: any;
 }
 
-export interface CategoryStoryblok {
-  seo?: SeoStoryblok[];
-  headline?: string;
-  image?: AssetStoryblok;
+export interface AwardStoryblok {
+  title?: string;
+  year?: string;
   description?: string;
-  grid?: boolean;
+  project_name?: string;
+  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  external_link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  thumbnail?: AssetStoryblok;
+  client_name?: string;
   _uid: string;
-  component: "category";
+  component: "award";
+  [k: string]: any;
+}
+
+export interface AwardsStoryblok {
+  items?: AwardStoryblok[];
+  _uid: string;
+  component: "awards";
   [k: string]: any;
 }
 
@@ -78,148 +89,202 @@ export interface RichtextStoryblok {
   [k: string]: any;
 }
 
-export interface ConfigStoryblok {
-  robots_txt_content?: string;
-  site_url?: string;
-  google_analytics_code?: string;
-  google_tag_manager?: string;
-  default_post_image: AssetStoryblok;
-  posts_per_page?: string;
-  header_nav?: NavItemStoryblok[];
-  social_items?: SocialItemStoryblok[];
-  footer_text?: RichtextStoryblok;
-  logo?: AssetStoryblok;
+export interface CareerStoryblok {
   title?: string;
+  content?: RichtextStoryblok;
+  _uid: string;
+  component: "career";
+  [k: string]: any;
+}
+
+export interface ConfigStoryblok {
+  logo?: AssetStoryblok;
+  header_nav?: NavItemStoryblok[];
+  address?: string;
+  phone?: string;
+  mail?: string;
+  facebook?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  twitter?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  instagram?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  pinterest?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
   _uid: string;
   component: "config";
   [k: string]: any;
 }
 
-export type MultilinkStoryblok =
-  | {
-      id?: string;
-      cached_url?: string;
-      anchor?: string;
-      linktype?: "story";
-      target?: "_self" | "_blank";
-      story?: {
-        name: string;
-        created_at?: string;
-        published_at?: string;
-        id: number;
-        uuid: string;
-        content?: {
-          [k: string]: any;
-        };
-        slug: string;
-        full_slug: string;
-        sort_by_date?: null | string;
-        position?: number;
-        tag_list?: string[];
-        is_startpage?: boolean;
-        parent_id?: null | number;
-        meta_data?: null | {
-          [k: string]: any;
-        };
-        group_id?: string;
-        first_published_at?: string;
-        release_id?: null | number;
-        lang?: string;
-        path?: null | string;
-        alternates?: any[];
-        default_full_slug?: null | string;
-        translated_slugs?: null | any[];
-        [k: string]: any;
-      };
-      [k: string]: any;
-    }
-  | {
-      url?: string;
-      cached_url?: string;
-      anchor?: string;
-      linktype?: "asset" | "url";
-      target?: "_self" | "_blank";
-      [k: string]: any;
-    }
-  | {
-      email?: string;
-      linktype?: "email";
-      target?: "_self" | "_blank";
-      [k: string]: any;
-    };
+export interface ContactStoryblok {
+  address?: string;
+  phone?: string;
+  email?: string;
+  image?: AssetStoryblok;
+  _uid: string;
+  component: "contact";
+  [k: string]: any;
+}
 
 export interface ContentStoryblok {
-  text?: string;
-  button?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  text?: RichtextStoryblok;
+  image?: AssetStoryblok;
   _uid: string;
   component: "content";
   [k: string]: any;
 }
 
-export interface LastPostsStoryblok {
-  headline?: string;
-  number_of_posts?: string;
-  grid?: boolean;
+export interface ImageFieldsStoryblok {
+  image?: AssetStoryblok;
+  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  name?: string;
+  date?: string;
   _uid: string;
-  component: "last-posts";
+  component: "image-fields";
   [k: string]: any;
 }
 
 export interface NavItemStoryblok {
-  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  sub_menu?: NavItemStoryblok[];
   label?: string;
-  location?: "" | "header" | "footer";
+  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  is_submenu?: boolean;
   _uid: string;
   component: "nav-item";
   [k: string]: any;
 }
 
 export interface PageStoryblok {
+  headline?: string;
   body?: (
-    | AllPostsStoryblok
-    | AllProjectsStoryblok
-    | AuthorStoryblok
-    | CategoryStoryblok
+    | ArchiectEyesStoryblok
+    | ArchitectEyeStoryblok
+    | AwardStoryblok
+    | AwardsStoryblok
+    | CareerStoryblok
     | ConfigStoryblok
+    | ContactStoryblok
     | ContentStoryblok
-    | LastPostsStoryblok
+    | ImageFieldsStoryblok
     | NavItemStoryblok
     | PageStoryblok
-    | PostStoryblok
+    | PeriodicalStoryblok
+    | PeriodicalsStoryblok
+    | ProductStoryblok
+    | ProductSerieStoryblok
+    | ProfileStoryblok
+    | ProfilesStoryblok
     | ProjectStoryblok
+    | PublicationStoryblok
+    | PublicationsStoryblok
     | SeoStoryblok
-    | SocialItemStoryblok
-    | TagStoryblok
+    | SlideshowStoryblok
+    | SpaceStoryblok
   )[];
-  seo?: SeoStoryblok[];
   _uid: string;
   component: "page";
   uuid?: string;
   [k: string]: any;
 }
 
-export interface PostStoryblok {
-  headline?: string;
-  image?: AssetStoryblok;
-  teaser?: string;
-  md_content?: string;
+export interface PeriodicalStoryblok {
+  title?: string;
+  year?: string;
+  periodical_name?: string;
+  author?: string;
+  photographer?: string;
   url?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
-  author?: StoryblokStory<AuthorStoryblok> | string;
-  categories?: (StoryblokStory<CategoryStoryblok> | string)[];
-  tags?: (StoryblokStory<TagStoryblok> | string)[];
-  seo?: SeoStoryblok[];
+  thumbnail?: AssetStoryblok;
   _uid: string;
-  component: "post";
+  component: "periodical";
   [k: string]: any;
 }
 
+export interface PeriodicalsStoryblok {
+  items?: PeriodicalStoryblok[];
+  _uid: string;
+  component: "periodicals";
+  [k: string]: any;
+}
+
+export interface ProductStoryblok {
+  headline?: string;
+  subtitle?: string;
+  text?: RichtextStoryblok;
+  image?: AssetStoryblok;
+  product_series?: ProductSerieStoryblok[];
+  seo?: SeoStoryblok[];
+  _uid: string;
+  component: "product";
+  [k: string]: any;
+}
+
+export interface ProductSerieStoryblok {
+  title?: string;
+  text?: RichtextStoryblok;
+  images?: ImageFieldsStoryblok[];
+  _uid: string;
+  component: "product-serie";
+  [k: string]: any;
+}
+
+export interface ProfileStoryblok {
+  photo?: AssetStoryblok;
+  description?: RichtextStoryblok;
+  _uid: string;
+  component: "profile";
+  [k: string]: any;
+}
+
+export interface ProfilesStoryblok {
+  title?: string;
+  items?: ProfileStoryblok[];
+  _uid: string;
+  component: "profiles";
+  [k: string]: any;
+}
+
+export type MultiassetStoryblok = {
+  alt?: string;
+  copyright?: string;
+  id: number;
+  filename: string;
+  name: string;
+  title?: string;
+  [k: string]: any;
+}[];
+
 export interface ProjectStoryblok {
   headline?: string;
-  image?: AssetStoryblok;
-  description?: string;
-  url?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  brief?: RichtextStoryblok;
+  solution?: RichtextStoryblok;
+  slideshow?: MultiassetStoryblok;
+  project_code?: string;
+  client_name?: string;
+  awards?: AwardStoryblok[];
+  press?: PublicationStoryblok[];
+  photo_credits?: string;
+  architect?: string;
+  seo?: SeoStoryblok[];
   _uid: string;
   component: "project";
+  [k: string]: any;
+}
+
+export interface PublicationStoryblok {
+  date?: string;
+  title?: string;
+  author?: string;
+  url?: string;
+  thumbnail?: AssetStoryblok;
+  client_name?: string;
+  _uid: string;
+  component: "publication";
+  [k: string]: any;
+}
+
+export interface PublicationsStoryblok {
+  book_cover?: AssetStoryblok;
+  purchase_link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  items?: PublicationStoryblok[];
+  _uid: string;
+  component: "publications";
   [k: string]: any;
 }
 
@@ -230,29 +295,24 @@ export interface SeoStoryblok {
   og_description?: string;
   twitter_title?: string;
   twitter_description?: string;
-  canonical_url?: string;
   twitter_image?: AssetStoryblok;
-  no_follow?: boolean;
   _uid: string;
   component: "seo";
   [k: string]: any;
 }
 
-export interface SocialItemStoryblok {
-  name?: string;
-  icon?: AssetStoryblok;
-  url?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+export interface SlideshowStoryblok {
+  images?: MultiassetStoryblok;
   _uid: string;
-  component: "social-item";
+  component: "slideshow";
   [k: string]: any;
 }
 
-export interface TagStoryblok {
-  seo?: SeoStoryblok[];
+export interface SpaceStoryblok {
   headline?: string;
-  description?: string;
-  grid?: boolean;
+  subtitle?: string;
+  images?: ImageFieldsStoryblok[];
   _uid: string;
-  component: "tag";
+  component: "space";
   [k: string]: any;
 }
