c980fb82149182fdad2e0db76ea7d416ff11ce4b
Alexandra Spalato <alexaspalato@gmail.com>
2024-05-09 17:19:24 +0200

    env vars

--- app/root.tsx
+++ app/root.tsx
@@ -4,37 +4,40 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  json,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Feature from "./components/Feature";
-import Grid from "./components/Grid";
-import Page from "./components/Page";
-import Teaser from "./components/Teaser";
+import styles from "./tailwind.css?url";
 
-const components = {
-  feature: Feature,
-  grid: Grid,
-  teaser: Teaser,
-  page: Page,
-};
+const isServer = typeof window === "undefined";
+
+const accessToken = isServer
+  ? process.env.STORYBLOK_PREVIEW_TOKEN
+  : //@ts-ignore
+    window.env.STORYBLOK_PREVIEW_TOKEN;
+
+const components = {};
 
 storyblokInit({
-  accessToken: "4hK4f9QPB5QjLdkuqhbRDQtt",
+  accessToken,
   use: [apiPlugin],
   components,
 });
 
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
-
-import styles from "./tailwind.css?url";
-
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-];
-
+export const loader = async () => {
+  return json({
+    env: {
+      STORYBLOK_PREVIEW_TOKEN: process.env.STORYBLOK_PREVIEW_TOKEN,
+    },
+  });
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { env } = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -45,6 +48,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.env = ${JSON.stringify(env)}`,
+          }}
+        />
         <ScrollRestoration />
         <Scripts />
       </body>
