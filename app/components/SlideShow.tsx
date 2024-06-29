a396095ec9da8a1e6f107f259181fbfed8bd7e9e
Alexandra Spalato <alexaspalato@gmail.com>
2024-06-29 15:21:28 +0200

    slideshow thumbnails

--- app/components/SlideShow.tsx
+++ app/components/SlideShow.tsx
@@ -4,9 +4,11 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
+  CarouselApi,
 } from "~/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Fade from "embla-carousel-fade";
+import { useState, useMemo, useEffect } from "react";
 
 export const SlideShow = ({
   images,
@@ -14,41 +16,107 @@ export const SlideShow = ({
   location = "project",
   ...props
 }) => {
-  return (
-    <Carousel
-      plugins={[
-        Autoplay({
-          delay: 4000,
-        }),
-        Fade(),
-      ]}
-      opts={{
-        loop: true,
-        align: "center",
-        containScroll: false,
-      }}
-    >
-      <CarouselContent>
-        {images.map((image) => (
-          <CarouselItem
-            key={image.url}
-            className={`flex justify-center ${props.className}`}
+  const [mainApi, setMainApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+
+  const mainImage = useMemo(
+    () =>
+      images.map((image, index) => (
+        <CarouselItem
+          key={index}
+          className={`flex justify-center align-bottom ${props.className}`}
+        >
+          <div
+            className={`${
+              location !== "home"
+                ? "bg-white flex items-center justify-center"
+                : ""
+            }`}
+            style={{
+              width: location !== "home" ? size.split("x")[0] + "px" : "auto",
+              height: location !== "home" ? size.split("x")[1] + "px" : "auto",
+            }}
           >
             <img
-              src={`${image.filename}/m/${size}`}
+              src={`${image.filename}/m/${location === "home" ? size : ""}`}
               alt={image.alt_text}
-              className="w-full h-auto object-cover"
+              className={`${
+                location !== "home"
+                  ? "max-w-full max-h-full"
+                  : "w-full h-auto object-cover"
+              }`}
             />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
+          </div>
+        </CarouselItem>
+      )),
+    [images, location, size]
+  );
 
-      {location != "home" && (
-        <CarouselPrevious className="absolute left-4 top-1/2 transform -translate-y-1/2 text-slate-500" />
+  const handleThumbnailClick = (index: number) => {
+    mainApi?.scrollTo(index);
+    setCurrent(index);
+  };
+
+  useEffect(() => {
+    if (mainApi) {
+      mainApi.on("select", (index) => {
+        setCurrent(index);
+      });
+    }
+  }, [mainApi]);
+
+  return (
+    <>
+      <Carousel
+        setApi={setMainApi}
+        plugins={[
+          Autoplay({
+            delay: 4000,
+          }),
+          Fade(),
+        ]}
+        opts={{
+          loop: true,
+          align: "center",
+          containScroll: false,
+        }}
+      >
+        <CarouselContent>{mainImage}</CarouselContent>
+
+        {location != "home" && (
+          <CarouselPrevious className="absolute left-4 top-1/2 transform -translate-y-1/2 text-slate-500" />
+        )}
+        {location != "home" && (
+          <CarouselNext className="absolute right-4 top-1/2 transform -translate-y-1/2 text-slate-500" />
+        )}
+      </Carousel>
+      {location !== "home" && (
+        <div className="flex gap-5 mt-4 flex-wrap">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`relative cursor-pointer ${
+                index === current ? "opacity-60" : ""
+              }`}
+              onClick={() => handleThumbnailClick(index)}
+            >
+              <div
+                className={`bg-white flex items-center justify-center`}
+                style={{
+                  width: "135px",
+                  height: "135px",
+                }}
+              >
+                <img
+                  src={`${image.filename}/m/135x135`}
+                  alt={image.alt_text}
+                  className="max-w-full max-h-full"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       )}
-      {location != "home" && (
-        <CarouselNext className="absolute right-4 top-1/2 transform -translate-y-1/2 text-slate-500" />
-      )}
-    </Carousel>
+    </>
   );
 };
