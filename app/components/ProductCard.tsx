2ba55e850db358738ab6e3009aec75c72054c3ba
Alexandra Spalato <alexaspalato@gmail.com>
2024-07-08 15:45:56 +0200

    productcard img size

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,5 +1,5 @@
-import { Link } from "@remix-run/react";
-import { ProductStoryblok } from "~/types";
+import { Link } from '@remix-run/react';
+import { ProductStoryblok } from '~/types';
 
 export const ProductCard = ({ product }: { product: ProductStoryblok }) => {
   const { headline, image, full_slug, subtitle } = product;
@@ -7,7 +7,7 @@ export const ProductCard = ({ product }: { product: ProductStoryblok }) => {
   return (
     <div className="relative overflow-hidden">
       <Link to={`/${full_slug}`}>
-        <img src={image?.filename} alt={image?.alt_text} />
+        <img src={`${image?.filename}/m/600x400`} alt={image?.alt_text} />
         <div className="absolute inset-0 bg-white bg-opacity-60 opacity-0 hover:opacity-100 transition-opacity duration-500 flex flex-col p-5 uppercase text-primary">
           <h2>{headline}</h2>
           <div className="text-sm">{subtitle}</div>
