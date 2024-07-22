88e4c842f0a95a76b1bc04ce3c50d37f89e19f40
Alexandra Spalato <alexaspalato@gmail.com>
2024-07-22 21:10:12 +0200

    carousel image size

--- app/components/LightBoxCarousel.tsx
+++ app/components/LightBoxCarousel.tsx
@@ -38,7 +38,7 @@ export const LightboxCarousel = ({
                     location === 'product'
                       ? item?.image?.filename
                       : item?.filename
-                  }/m/1440x0`}
+                  }/m/0x800`}
                   className="w-full h-auto max-h-full object-contain"
                 />
               </div>
