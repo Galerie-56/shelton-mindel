b518d0003a5b315ffd3850724a7c8c11f1c26efa
Alexandra Spalato <alexaspalato@gmail.com>
2024-07-23 14:29:12 +0200

    profile name black

--- app/components/bloks/Profile.tsx
+++ app/components/bloks/Profile.tsx
@@ -1,6 +1,6 @@
-import { storyblokEditable } from "@storyblok/react";
-import type { ProfileStoryblok } from "~/types";
-import { renderRichText } from "@storyblok/react";
+import { storyblokEditable } from '@storyblok/react';
+import type { ProfileStoryblok } from '~/types';
+import { renderRichText } from '@storyblok/react';
 
 export const Profile = ({ blok }: ProfileStoryblok) => {
   const { _uid, description, photo } = blok;
@@ -8,7 +8,7 @@ export const Profile = ({ blok }: ProfileStoryblok) => {
     <div
       {...storyblokEditable(blok)}
       key={_uid}
-      className="flex flex-col md:flex-row justify-between items-start  py-4 md:space-x-5"
+      className="flex flex-col md:flex-row justify-between items-start  py-4 md:space-x-5 profile"
     >
       <div className="mb-2 md:mb-0 md:w-1/3">
         <img
